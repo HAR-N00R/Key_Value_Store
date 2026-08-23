9741db6d57f1d6a4a95349e61879731d8ca8d076
sandhu <hsingh135@horizon.csueastbay.edu>
2026-08-23 14:51:49 -0700

    Changed to command parsing

--- main.cpp
+++ main.cpp
@@ -1,149 +1,103 @@
 #include <iostream>
-#include <limits>
 #include <stdexcept>
 #include "KeyValueStore.h"
 
-void separator() {
-    std::cout << std::string(60, '=') << std::endl;
-}
+void parsing(std::string input, std::string& command, std::string& key, std::string& value) {
 
-template <typename datatype>
-void inputValidator(datatype& input) {
-    std::cout << "Input : ";
-    std::cin >> input;
-    while (std::cin.fail()) {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "invalid input, Please reenter" << std::endl << "Input: ";
-        std::cin >> input;
+    std::size_t space = input.find(' ');
+    if (space != std::string::npos) {
+        command = input.substr(0, space);
+        input.erase(0, space + 1);
     }
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << std::string(60, '=') << std::endl;
+    else {
+        command = input;
+        return;
+    }
+    if (input.empty()) {
+        throw std::runtime_error("Key missing");
+    }
+    space = input.find(' ');
+    if (space != std::string::npos) {
+        key = input.substr(0, space);
+        input.erase(0, space + 1);
+    }
+    else {
+        key = input;
+        return;
+    }
+    if (!input.empty()) {
+        value = input;
+        input.clear();
+    }
 }
 
-void printMessage(const std::string& message) {
-    separator();
-    std::cout << message << std::endl;
-    separator();
-}
-
-int menu() {
+void commandParser(KeyValueStore& kvs) {
     while (true) {
-        std::cout << "Please select one of the following operations:" << std::endl;
-        std::cout << "1. Create new key value pair" << std::endl;
-        std::cout << "2. Check if key value pair exists" << std::endl;
-        std::cout << "3. Look up key value pair" << std::endl;
-        std::cout << "4. Delete key value pair" << std::endl;
-        std::cout << "5. Quit" << std::endl;
+        std::string input;
 
-        int choice;
-        inputValidator(choice);
-
-        if (choice >= 1 && choice <= 5) {
-            return choice;
-        }
-        std::cout << "Invalid Choice" << std::endl;
-        separator();
-    }
-}
-
-void addKeyValue(KeyValueStore& kvs) {
-    std::cout << "Enter the key and value to be stored" << std::endl;
-    std::string key;
-    std::string value;
-    std::cout << "Key: ";
-    getline(std::cin, key);
-    std::cout << "Value: ";
-    getline(std::cin, value);
-    if (!key.empty()) {
-        if (kvs.exists(key)) {
-            printMessage("Key successfully updated");
-        }
-        else {
-            printMessage("Key successfully created and stored");
-        }
-        kvs.setValue(key, value);
-    }
-    else {
-        printMessage("Key cannot be empty");
-    }
-}
-
-void keyExists(const KeyValueStore& kvs) {
-    std::cout << "Enter the key to check" << std::endl;
-    std::string key;
-    std::cout << "Key: ";
-    getline(std::cin, key);
-    if (!key.empty()) {
-        if (kvs.exists(key)) {
-            printMessage("Key exists");
+        std::string command;
+        std::string key;
+        std::string value;
+        std::cout << "Input command: ";
+        getline(std::cin, input);
+        if (input == "quit" || input == "QUIT") {
             return;
         }
-        printMessage("Key doesn't exist");
-    }
-    else {
-        printMessage("Key cannot be empty");
-    }
-}
-
-void keyLookup(const KeyValueStore& kvs) {
-    std::cout << "Enter the key to look up" << std::endl;
-    std::string key;
-    std::cout << "Key: ";
-    getline(std::cin, key);
-    if (!key.empty()) {
         try {
-            std::string value = "Value: " + kvs.getValue(key);
-            printMessage(value);
+            parsing(input, command, key, value);
+            if (key.empty()) {
+                throw std::runtime_error("Invalid command");
+            }
+            if ((command != "set" && command != "SET") && !value.empty()) {
+                throw std::runtime_error("Invalid command");
+            }
+
+            if (command == "set" || command == "SET") {
+                kvs.setValue(key, value);
+            }
+            else if (command == "get" || command == "GET") {
+                try {
+                    std::string value = kvs.getValue(key);
+                    std::cout << "Key: " << key << std::endl;
+                    std::cout << "Value: " << value << std::endl;
+                }
+                catch (const std::out_of_range& e) {
+                    std::cout << "Key doesn't exist" << std::endl;
+                }
+            }
+            else if (command == "exists" || command == "EXISTS") {
+                if (kvs.exists(key)) {
+                    std::cout << "Key exists" << std::endl;
+                }
+                else {
+                    std::cout << "Key doesn't exist" << std::endl;
+                }
+            }
+            else if (command == "delete" || command == "DELETE") {
+                if (kvs.removeKey(key)) {
+                    std::cout << "Key deleted" << std::endl;
+                }
+                else {
+                    std::cout << "Unable to delete" << std::endl;
+                }
+            }
+            else {
+                std::cout << "Invalid Command" << std::endl;
+            }
         }
-        catch (const std::out_of_range& e) {
-            printMessage("Key doesn't exist");
+        catch (const std::runtime_error& error) {
+            std::cout << "Invalid command" << std::endl;
         }
-    }
-    else {
-        printMessage("Key cannot be empty");
     }
 }
-
-void deleteKeyValue(KeyValueStore& kvs) {
-    std::cout << "Enter the key to delete" << std::endl;
-    std::string key;
-    std::cout << "Key: ";
-    getline(std::cin, key);
-    if (!key.empty()) {
-        if (kvs.removeKey(key)) {
-            printMessage("Key Value pair deleted");
-            return;
-        }
-        printMessage("Unable to delete");
-    }
-    else {
-        printMessage("Key cannot be empty");
-    }
-}
-
 
 int main() {
-    printMessage("Welcome to Key Value Store");
+    std::cout << std::string(60, '=') << std::endl;
+    std::cout << "Welcome to Key Value Store" << std::endl;
+    std::cout << std::string(60, '=') << std::endl;
 
     KeyValueStore kvs;
 
-    while (true) {
-        switch (menu()) {
-        case 1:
-            addKeyValue(kvs);
-            break;
-        case 2:
-            keyExists(kvs);
-            break;
-        case 3:
-            keyLookup(kvs);
-            break;
-        case 4:
-            deleteKeyValue(kvs);
-            break;
-        case 5:
-            return 0;
-        }
-    }
+    commandParser(kvs);
+    return 0;
 }
