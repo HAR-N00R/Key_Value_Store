3ff72a67436c2144dbe59fefdec2d66333d5aa3a
sandhu <hsingh135@horizon.csueastbay.edu>
2026-09-14 01:58:01 -0700

    added additional tests

--- tests/ServerConcurrencyTests.cpp
+++ tests/ServerConcurrencyTests.cpp
@@ -7,6 +7,11 @@
 #include <thread>
 #include <sstream>
 #include "Network/Socket.h"
+#include "Server/Server.h"
+#include "KeyValueStore/KeyValueStore.h"
+#include <filesystem>
+#include <atomic>
+#include <vector>
 
 constexpr std::size_t MAX_FRAME_SIZE = 1024 * 1024 * 20;
 
@@ -58,7 +63,6 @@ std::string sendFrame(int fd, const std::string& request) {
             throw std::runtime_error("Failed to send data to client");
         }
     }
-    std::size_t totalSent = 0;
     for (std::size_t i = 0; i < requestSize; i++) {
         ssize_t sendSize = send(fd, request.data() + i, 1, 0);
         if (sendSize == -1) {
@@ -67,7 +71,6 @@ std::string sendFrame(int fd, const std::string& request) {
         if (sendSize == 0) {
             throw std::runtime_error("Failed to send data to client");
         }
-        totalSent += static_cast<std::size_t>(sendSize);
     }
 
     return receiveFrame(fd);
@@ -85,7 +88,6 @@ void concurrencyTest(int num) {
     if (connect(fd, reinterpret_cast<sockaddr*>(&serveraddr_in), sizeof(serveraddr_in)) == -1) {
         throw std::runtime_error("Failed to connect to server");
     }
-    std::cout << "Connection established" << std::endl;
     for (int i = num; i < num + 100; i++) {
         std::string request = "set " + std::to_string(i) + " " + std::to_string(i);
         sendFrame(fd, request);
@@ -110,13 +112,82 @@ bool dataValidityTest() {
         if (response == "Key doesn't exist") {
             return false;
         }
-        std::stringstream ss(response);
-        std::string key;
-        std::string value;
-        std::getline(ss, key, ':');
-        std::getline(ss, value);
-        value.erase(0, 1);
-        if (key != value) {
+        std::string expected = std::to_string(i) + ": " + std::to_string(i);
+        if (expected != response) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void valuePersistentTest(const std::string& key, const std::string& value) {
+    Socket client(socket(AF_INET, SOCK_STREAM, 0));
+    int fd = client.getSocket();
+    sockaddr_in serveraddr_in{};
+    serveraddr_in.sin_family = AF_INET;
+    serveraddr_in.sin_port = htons(8080);
+    if (inet_pton(AF_INET, "127.0.0.1", &serveraddr_in.sin_addr) <= 0) {
+        throw std::runtime_error("Invalid server address");
+    }
+    if (connect(fd, reinterpret_cast<sockaddr*>(&serveraddr_in), sizeof(serveraddr_in)) == -1) {
+        throw std::runtime_error("Failed to connect to server");
+    }
+    for (int i = 0; i < 100; i++) {
+        std::string request = "set " + key + " " + value;
+        sendFrame(fd, request);
+    }
+}
+
+std::string checkValuePersistence(const std::string& key) {
+    Socket client(socket(AF_INET, SOCK_STREAM, 0));
+    int fd = client.getSocket();
+    sockaddr_in serveraddr_in{};
+    serveraddr_in.sin_family = AF_INET;
+    serveraddr_in.sin_port = htons(8080);
+    if (inet_pton(AF_INET, "127.0.0.1", &serveraddr_in.sin_addr) <= 0) {
+        throw std::runtime_error("Invalid server address");
+    }
+    if (connect(fd, reinterpret_cast<sockaddr*>(&serveraddr_in), sizeof(serveraddr_in)) == -1) {
+        throw std::runtime_error("Failed to connect to server");
+    }
+    std::string request = "get " + key;
+    std::string response = sendFrame(fd, request);
+    if (response == "Key doesn't exist") {
+        return response;
+    }
+    std::stringstream ss(response);
+    std::string receivedKey;
+    std::string receivedValue;
+    std::getline(ss, receivedKey, ':');
+    std::getline(ss, receivedValue);
+    receivedValue.erase(0, 1);
+    return receivedValue;
+}
+
+void compactRequest() {
+    Socket client(socket(AF_INET, SOCK_STREAM, 0));
+    int fd = client.getSocket();
+    sockaddr_in serveraddr_in{};
+    serveraddr_in.sin_family = AF_INET;
+    serveraddr_in.sin_port = htons(8080);
+    if (inet_pton(AF_INET, "127.0.0.1", &serveraddr_in.sin_addr) <= 0) {
+        throw std::runtime_error("Invalid server address");
+    }
+    if (connect(fd, reinterpret_cast<sockaddr*>(&serveraddr_in), sizeof(serveraddr_in)) == -1) {
+        throw std::runtime_error("Failed to connect to server");
+    }
+    std::string request = "compact";
+    sendFrame(fd, request);
+}
+
+bool compactionValidation(KeyValueStore& store) {
+    for (int i = 0; i < 800; i++) {
+        std::string key = std::to_string(i);
+        std::string response = store.getValue(key);
+        if (response == "Key doesn't exist") {
+            return false;
+        }
+        if (key != response) {
             return false;
         }
     }
@@ -124,26 +195,77 @@ bool dataValidityTest() {
 }
 
 int main() {
-    std::thread clientThread1(concurrencyTest, 0);
-    std::thread clientThread2(concurrencyTest, 100);
-    std::thread clientThread3(concurrencyTest, 200);
-    std::thread clientThread4(concurrencyTest, 300);
-    std::thread clientThread5(concurrencyTest, 400);
-    std::thread clientThread6(concurrencyTest, 500);
-    std::thread clientThread7(concurrencyTest, 600);
-    std::thread clientThread8(concurrencyTest, 700);
-    clientThread1.join();
-    clientThread2.join();
-    clientThread3.join();
-    clientThread4.join();
-    clientThread5.join();
-    clientThread6.join();
-    clientThread7.join();
-    clientThread8.join();
-    if (!dataValidityTest()) {
-        std::cout << "Data validity test failed" << std::endl;
+    std::filesystem::remove("test_data.db");
+    std::string message;
+    std::string valueBeforeRestart;
+    std::string valueAfterRestart;
+    std::atomic_bool serverIsRunning(false);
+    {
+        std::thread serverThread([&serverIsRunning]()
+        {
+            Server server("test_data.db");
+            serverIsRunning.store(true);
+            serverIsRunning.notify_one();
+            std::vector<std::thread> clientThreads;
+            for (int i = 0; i < 15; i++) {
+                int fd = server.acceptSocket();
+                clientThreads.emplace_back([&server, fd]()
+                {
+                    Socket client(fd);
+                    server.handleClient(client);
+                });
+            }
+            for (auto& thread : clientThreads) {
+                thread.join();
+            }
+        });
+        serverIsRunning.wait(false);
+        std::thread clientThread1(concurrencyTest, 0);
+        std::thread clientThread2(concurrencyTest, 100);
+        std::thread clientThread3(concurrencyTest, 200);
+        std::thread clientThread4(concurrencyTest, 300);
+        std::thread clientThread5(concurrencyTest, 400);
+        std::thread clientThread6(concurrencyTest, 500);
+        std::thread clientThread7(concurrencyTest, 600);
+        std::thread clientThread8(concurrencyTest, 700);
+        clientThread1.join();
+        clientThread2.join();
+        clientThread3.join();
+        clientThread4.join();
+        clientThread5.join();
+        clientThread6.join();
+        clientThread7.join();
+        clientThread8.join();
+        if (!dataValidityTest()) {
+            message += "Data validity test failed\n";
+        }
+        std::thread clientThread9(valuePersistentTest, "testKey", "1234");
+        std::thread clientThread10(valuePersistentTest, "testKey", "4321");
+        std::thread clientThread11(valuePersistentTest, "testKey", "5678");
+        std::thread clientThread12(valuePersistentTest, "testKey", "8765");
+        clientThread9.join();
+        clientThread10.join();
+        clientThread11.join();
+        clientThread12.join();
+        valueBeforeRestart = checkValuePersistence("testKey");
+        compactRequest();
+        serverThread.join();
+    }
+
+    KeyValueStore store("test_data.db");
+    valueAfterRestart = store.getValue("testKey");
+    if (valueBeforeRestart != valueAfterRestart) {
+        message += "Value persistence test failed";
+    }
+    if (!compactionValidation(store) || valueBeforeRestart != valueAfterRestart) {
+        message += "Data compaction test failed\n";
+    }
+    if (message.empty()) {
+        std::cout << "All Server concurrency tests passed" << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << message << std::endl;
         return 1;
     }
-    std::cout << "Data validity test passed" << std::endl;
-    return 0;
 }
