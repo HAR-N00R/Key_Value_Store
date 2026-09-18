555f103e4a34b85e1b5d8b7e4a175df693063b61
sandhu <hsingh135@horizon.csueastbay.edu>
2026-09-17 19:02:03 -0700

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,18 @@ int main() {
     std::cout << std::string(60, '=') << std::endl;
 
     Server server("data.db");
-    server.run();
+    std::thread serverThread([&server]()
+    {
+        server.run();
+    });
+    int stopTest;
+    std::cin >> stopTest;
+    if (stopTest == 1) {
+        std::cout << std::string(60, '=') << std::endl;
+        server.stop();
+        std::cout << std::string(60, '-') << std::endl;
+    }
+    serverThread.join();
 
     return 0;
 }
